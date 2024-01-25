776b0b3dfb4200253103fb6f73a1cfd7cf246ff2
Lera Demina <lera.demia.12@mail.ru>
2024-01-25 19:03:10 +0300

    first-com

---
+++ app/src/main/java/ru/itmo/notes/EditActivity.kt
@@ -0,0 +1,11 @@
+package ru.itmo.notes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit)
+    }
+}
--- app/src/main/java/ru/itmo/notes/MainActivity.kt
+++ app/src/main/java/ru/itmo/notes/MainActivity.kt
@@ -1,11 +1,39 @@
 package ru.itmo.notes
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
+import ru.itmo.notes.db.MyDbManager
 
 class MainActivity : ComponentActivity() {
+    val myDbManager = MyDbManager(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // TODO(): implement here
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        myDbManager.openDb()
+        val dataList = myDbManager.readDbData()
+       /* for (item in dataList) {
+            text.toString(item)
+            text.toString("\n")
+        }*/
+    }
+
+    fun onClickNew(view: TextView) {
+        /*tvTest.text = ""
+        myDbManager.insertToDb(edTitle.text.toString(), edContent.text.toString())
+        val dataList = myDbManager.readDbData()
+        for (item in dataList) {
+            tvTest.append(item)
+            tvTest.append("\n")
+        }*/
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        myDbManager.closeDb()
     }
 }
